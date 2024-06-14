d9e5c658f00df9de1809eec905efc0f9d603d0ce
NerokiStage <nerokikanye@gmail.com>
2024-06-14 22:31:01 +0000

    lineage_r8q: Fix  Boot Animation Resolution Flag
    Fix boot animation resolution by setting TARGET_BOOT_ANIMATION_RES flag to 1080.

--- lineage_r8q.mk
+++ lineage_r8q.mk
@@ -27,8 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_r8q
